3918f50fa852d5ffe6f1080b8e34c29a3cfc607d
willyyyyc <willcarmichaelcf@gmail.com>
2023-12-26 00:09:53 -0400

    fix broken course code bug in prereq list

--- test.py
+++ test.py
@@ -4,27 +4,47 @@ import re
 #break line into list of characters
 #goal: a list of course title objects to fill an adjacency list. some entries in list will themselves be lists, representing optional ('or' prerequisites)
 
-#change to re for each token
+def remove_item(list, item):
+    return [i for i in list if i != item]
+
+none = re.compile('None')
 faculty = re.compile('^[A-Z]{4}$')
 code = re.compile('^[0-9]{4}')
 or_re = re.compile('or')
 and_re = re.compile('and')
-#need to separated the braces from the tokens
 left_brace = re.compile('\(')
 right_brace = re.compile('\)')
 
-#heading = re.compile('Course Descriptions')
-#csci = re.compile('^CSCI\\s\\d{4}\\s[a-zA-Z]+')
 with open('test.txt', 'r') as f:
     for line in f:
         l = line.rsplit()
         bracesplit = re.compile('(\\()|(\\))').split
         l = [part for word in l for part in bracesplit(word) if part]
         print('->', l)
-        if 'None' in l:
-            print(l)
-        else:
-            patterns = [faculty, code, or_re, and_re, left_brace, right_brace]
-            matches = [word for word in l if any(pattern.match(word) for pattern in patterns)]
-            for match in matches:
-                print(match)
+
+        #x = [word for word in l if word != ('PREREQUISITES:')]
+        l = remove_item(l, 'PREREQUISITES:')
+        l = remove_item(l, ',')
+
+        broken_code_0 = re.compile('^[0-9]{3}$')
+        broken_code_1 = re.compile('^[0-9]')
+        #iter_l = iter(l)
+        #l = [i + next(iter_l, '') for i in iter_l if broken_code.match(i)]
+
+        # Assumes one occurence
+        i = 0
+        while i < len(l) - 1:
+            if broken_code_0.match(l[i]) and broken_code_1.match(l[i + 1]):
+                l[i] = l[i] + l[i + 1]
+                l.pop(i + 1)
+                break
+            i += 1
+
+
+        patterns = [none, faculty, code, or_re, and_re, left_brace, right_brace]
+        matches = [word for word in l if any(pattern.match(word) for pattern in patterns)]
+        for match in matches:
+            #make course title item
+            l.remove(match)
+
+        print('->', l)
